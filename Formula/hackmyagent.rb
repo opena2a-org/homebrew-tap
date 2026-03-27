533f09c7117ce3bdc49bf3bf81794c008c9aadd1
Abdel Fane <abdelsyfane@gmail.com>
2026-03-26 20:00:59 -0600

    hackmyagent 0.12.1

--- Formula/hackmyagent.rb
+++ Formula/hackmyagent.rb
@@ -1,8 +1,8 @@
 class Hackmyagent < Formula
   desc "Security scanner for AI agents - find vulnerabilities before attackers do"
   homepage "https://github.com/opena2a-org/hackmyagent"
-  url "https://registry.npmjs.org/hackmyagent/-/hackmyagent-0.12.0.tgz"
-  sha256 "bac5110929cf57c619a6d638382d0eebaf82254e5b3d9defac2fd5d0751b18ae"
+  url "https://registry.npmjs.org/hackmyagent/-/hackmyagent-0.12.1.tgz"
+  sha256 "a22492fd302bfca10c6fa11896f2a8a802d126036f937b969906900d65949418"
   license "Apache-2.0"
 
   depends_on "node"
